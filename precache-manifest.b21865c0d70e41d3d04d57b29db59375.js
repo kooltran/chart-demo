a107d6c99a009a59387d2307c80fceb7e9a34f58
it <it@its-Mac-mini-2.local>
2020-06-12 14:48:20 +0700

    Updates

---
+++ precache-manifest.b21865c0d70e41d3d04d57b29db59375.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07bd6c96a7742871cd2a9a4b8e1663d",
+    "url": "/chart-demo/index.html"
+  },
+  {
+    "revision": "25428e6f11d0b8182b72",
+    "url": "/chart-demo/static/css/main.92f29d4d.chunk.css"
+  },
+  {
+    "revision": "a5944ec1b9f18d0d9a31",
+    "url": "/chart-demo/static/js/2.229295f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chart-demo/static/js/2.229295f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25428e6f11d0b8182b72",
+    "url": "/chart-demo/static/js/main.9ff68168.chunk.js"
+  },
+  {
+    "revision": "5072ea4246731600ac08",
+    "url": "/chart-demo/static/js/runtime-main.7dd81717.js"
+  },
+  {
+    "revision": "207f857f1693f07cee6882ff84ccf8bc",
+    "url": "/chart-demo/static/media/bell.207f857f.svg"
+  },
+  {
+    "revision": "5fee6294ec1593733ec96cc94f64473e",
+    "url": "/chart-demo/static/media/cloud.5fee6294.svg"
+  },
+  {
+    "revision": "217a3d24c430c26ba1fe174853dfbf12",
+    "url": "/chart-demo/static/media/down_arrow.217a3d24.svg"
+  },
+  {
+    "revision": "ddaa6388f5e0151f230d0ff3e44356c2",
+    "url": "/chart-demo/static/media/moon.ddaa6388.svg"
+  },
+  {
+    "revision": "25e36826bf9e72a06dde5a79d29dbaf7",
+    "url": "/chart-demo/static/media/plus.25e36826.svg"
+  },
+  {
+    "revision": "da20b792869c96c5344584fcb27af663",
+    "url": "/chart-demo/static/media/sun.da20b792.svg"
+  },
+  {
+    "revision": "2db6e93469ca940b464fc3deebbcb869",
+    "url": "/chart-demo/static/media/temperature.2db6e934.svg"
+  },
+  {
+    "revision": "4d85c86eefad454a5153025d09d9394f",
+    "url": "/chart-demo/static/media/water_drop.4d85c86e.svg"
+  }
+]);
